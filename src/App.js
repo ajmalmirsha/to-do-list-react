4b777d91b1227242ab6e65eac0c91aba31a171b4
ajmalmirsha <ajmalmirshaa2004@gmail.com>
2023-03-30 20:44:17 +0530

    to-do-list initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+  const day = new Date().toLocaleString("default", {
+    weekday: "long",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {day} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>{setToDo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>{setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])}} className="fas fa-plus"></i>
+      </div>
+     
+     
+      <div className="todos">
+  {toDos.map((todo, index) => (
+    <div key={todo.id} className="todo">
+      <div className="left">
+        <input
+          type="checkbox"
+          checked={todo.status}
+          onChange={() => {
+            setToDos(
+              toDos.map((t, i) => {
+                if (i === index) {
+                  return { ...t, status: !t.status };
+                }
+                return t;
+              })
+            );
+          }}
+        />
+        <p>{todo.text}</p>
+        
+      </div>
+      <div className="left">
+          {todo.status ? <p id='completed'>completed</p> : null}
+          </div>
+      <div className="right">
+        <i
+          onClick={() => {
+            setToDos([...toDos.slice(0, index), ...toDos.slice(index + 1)]);
+          }}
+          className="fas fa-times"
+        ></i>
+ 
+      </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 }
